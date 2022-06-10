643407172f362fd56755424afed970c7dea1aef2
Maury PC <tryptic-st@outlook.com>
2022-06-10 13:26:59 -0500

    Add the required changes

--- src/index.js
+++ src/index.js
@@ -1,41 +1,44 @@
 /* eslint-disable no-unused-vars */
-import _ from 'lodash';
 import './style.css';
-import Data from './data.xml';
-import Notes from './data.csv';
 
 const section = document.querySelector('section');
 const taskInput = document.querySelector('input');
 const tasksContainer = document.querySelector('.tasks-container');
+const allTasksContainer = document.getElementById('tasks-container');
 const completedTasks = document.querySelector('button');
 
-class myList {
-  constructor(description, completed, index) {
-    this.description = description;
-    this.completed = completed;
-    this.index = index;
+/* class myList{
+    constructor(description, completed, index){
+        this.description = description;
+        this.completed = completed;
+        this.index = index;
+    }
   }
-}
-
-const listArray = [];
-const addTask = (taskValue) => {
+  */
+const list = [
+  {
+    description: 'wash dishes',
+    completed: true,
+    index: 1,
+  },
+  {
+    description: 'make the bed',
+    completed: true,
+    index: 2,
+  },
+];
+  /* const listArray = []; */
+list.forEach((Element) => {
   const newTaskContainer = document.createElement('div');
   newTaskContainer.classList.add('new-task--container');
   newTaskContainer.innerHTML += `
    <div class="left-content">
-      <input type="checkbox" class="check-task">
-      <span>${taskValue}</span>
+      <input type="checkbox" ${Element.completed} class="check-task">
+      <span>${Element.description}</span>
    </div>
    <div class="right-content">
       <i class="fa-solid fa-ellipsis-vertical"></i>
       <i class="fa-solid fa-trash-can"></i>
    </div>`;
-  tasksContainer.appendChild(newTaskContainer);
-};
-taskInput.addEventListener('keypress', (e) => {
-  if (e.key === 'Enter' && taskInput.value) {
-    e.preventDefault();
-    addTask(taskInput.value);
-    taskInput.value = null;
-  }
+  allTasksContainer.appendChild(newTaskContainer);
 });
